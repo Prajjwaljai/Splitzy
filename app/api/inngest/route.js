ffa63b2f80ec2c26bf37a17524c8c80f6c38b8ba
PRAJJWAL JAISWAL <prajjwalj885@gmail.com>
2026-09-08 18:25:37 +0530

    Fix2

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,15 +1,28 @@
+// import { serve } from "inngest/next";
+// import { inngest } from "@/lib/inngest/client";
+// import { paymentReminders } from "@/lib/inngest/payment-reminders";
+// import { spendingInsights } from "@/lib/inngest/spending-insights";
+
+// // Create an API that serves zero functions
+// export const { GET, POST, PUT } = serve({ //PUT remove by me
+//   client: inngest,
+//   functions: [
+//     /* your functions will be passed here later! */
+//     spendingInsights,
+//     paymentReminders,
+//   ],
+// });
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
 import { paymentReminders } from "@/lib/inngest/payment-reminders";
 import { spendingInsights } from "@/lib/inngest/spending-insights";
 
-// Create an API that serves zero functions
-export const { GET, POST, PUT } = serve({ //PUT remove by me
+export const maxDuration = 300;
+
+export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    /* your functions will be passed here later! */
     spendingInsights,
     paymentReminders,
   ],
 });
-
